582d078c201dc2298342e5027ec327efccde6dc4
Abhijit Fulsagar <abhijit@Abhijits-Mac.local>
2018-04-06 15:10:24 -0700

    basic required tableView methods add

--- ToDo/ToDoListViewController.swift
+++ ToDo/ToDoListViewController.swift
@@ -25,5 +25,15 @@ class ToDoListViewController: UITableViewController {
         cell.textLabel?.text=itemArray[indexPath.row]
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print( itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
